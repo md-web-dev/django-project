4ec52904abbcb4561ec243e7e21983ed46be120d
Joee dev <dev@192.168.10.2>
2022-09-21 23:40:24 +0500

    route for group stage change mapping

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from thb_interview_project_events.users.api.views import UserViewSet
-from core.views import CustomerViewSet, StageChangeEventViewSet, StageViewSet
+from core.views import CustomerViewSet, StageChangeEventViewSet, StageViewSet, GroupStageChangeMappingViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -13,6 +13,7 @@ router.register("users", UserViewSet)
 router.register("customers", CustomerViewSet)
 router.register("stage-change-events", StageChangeEventViewSet)
 router.register("stages", StageViewSet)
+router.register("group-stage-change-mapping", GroupStageChangeMappingViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
