e7e83d97ff08fa67276162dbeac1dcb9e9411e3c
Joee dev <dev@192.168.10.2>
2022-09-21 23:44:42 +0500

    group stage change view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import Customer, Stage, StageChangeEvent
-from .serializers import CustomerSerializer, StageSerializer, StageChangeEventSerializer
+from .models import Customer, Stage, StageChangeEvent, GroupStageChangeMapping
+from .serializers import CustomerSerializer, StageSerializer, StageChangeEventSerializer, GroupStageChangeMappingSerializer
 from . import utils
  
 class CustomerViewSet(viewsets.ModelViewSet):
@@ -20,7 +18,7 @@ class StageViewSet(viewsets.ModelViewSet):
     A viewset for viewing and editing stage instances.
     """
     serializer_class = StageSerializer
-    queryset = Stage.objects.all()   
+    queryset = Stage.objects.all()  
 
 class StageChangeEventViewSet(viewsets.ModelViewSet):
     """
@@ -32,7 +30,13 @@ class StageChangeEventViewSet(viewsets.ModelViewSet):
     # http://localhost:8000/api/stage-change-events/group_stage_change_events/
     @action(detail=False, methods=["POST"])
     def group_stage_change_events(self, request, pk=None):
-
+        mapping = request.data.get('mapping', None)
+        stage_change_counts = utils.group_stage_change_event_counts(mapping)
         return Response(
-            data=[], status=500
+            data=stage_change_counts, status=200
         )
+
+class GroupStageChangeMappingViewSet(viewsets.ModelViewSet):
+
+    serializer_class = GroupStageChangeMappingSerializer
+    queryset = GroupStageChangeMapping.objects.all().order_by('id')
